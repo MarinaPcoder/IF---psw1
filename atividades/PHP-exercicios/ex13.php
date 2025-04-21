589493bcf595187a91ef13a0ec35e05bdee7d8d4
Marinakk <marinaprado.learning@gmail.com>
2025-04-21 09:42:23 -0300

    ex13 feito

---
+++ atividades/PHP-exercicios/ex13.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 13</title>
+</head>
+<body>
+<!--de forma simples -->
+    <?php
+         echo "Olá, mundo!";
+    ?>
+
+    <br>
+
+<!--usando variável -->
+    <?php
+        $texto = "Seja bem-vindo!";
+        echo $texto;
+    ?>
+
+<!--css -->
+    <?php
+        $mensagem = "LALALLALALALALALLALALLALALA";
+        echo "<p style='color: #2B5288; font-weight: bold;'>$mensagem</p>";
+    ?>
+</body>
+</html>
+
+
